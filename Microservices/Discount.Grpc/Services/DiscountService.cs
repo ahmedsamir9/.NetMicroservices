1f8b9fa5d61e0cfc5d5aa0658a805976b58453fb
owner <owner@DESKTOP-BQ7FT65>
2023-01-30 23:27:38 +0200

    Add Grpc in server side

---
+++ Microservices/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,31 @@
+﻿using Discount.Grpc.Protos;
+using Grpc.Core;
+using System.Threading.Tasks;
+using static Discount.Grpc.Protos.DiscountService;
+
+namespace Discount.Grpc.Services
+{
+    public class DiscountServices : DiscountServiceBase
+    {
+        public override Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+        {
+            return base.CreateDiscount(request, context);
+        }
+
+        public override Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+        {
+            return base.DeleteDiscount(request, context);
+        }
+
+        public override Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+        {
+            return base.GetDiscount(request, context);
+        }
+
+        public override Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+        {
+            return base.UpdateDiscount(request, context);
+        }
+    }
+
+}
